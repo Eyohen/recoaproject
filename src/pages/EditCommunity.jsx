efbb2939c1a0edf80813470ad97b13b4b01c1fd2
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-07 05:13:56 +0100

    Fix bug in login functionality

--- src/pages/EditCommunity.jsx
+++ src/pages/EditCommunity.jsx
@@ -1,176 +1,135 @@
-import { useContext, useEffect, useState } from "react"
-// import Footer from "../components/Footer"
-// import Navbar from "../components/Navbar"
-import {ImCross} from 'react-icons/im'
-import axios from "axios"
-import { URL } from "../url"
-import { useNavigate, useParams } from "react-router-dom"
-import { UserContext } from "../context/UserContext"
+import { useContext, useEffect, useState } from "react";
+import { ImCross } from "react-icons/im";
+import axios from "axios";
+import { URL } from "../url";
+import { useNavigate, useParams } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
 import { SlArrowLeft } from "react-icons/sl";
 
-
 const EditCommunity = () => {
+  const CommunityId = useParams().id;
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [status, setStatus] = useState("");
+  const [location, setLocation] = useState("");
+  const [file, setFile] = useState(null);
+  const [imageUrl, setImageUrl] = useState(""); // State to hold the image URL
 
-    const apartmentId=useParams().id
-    const {user}=useContext(UserContext)
-    const navigate=useNavigate()
-    const [roomName,setRoomName]=useState("")
-    const [floorsAvailable,setFloorsAvailable]=useState("")
-    const [description,setDescription]=useState("")
-    const [bedroom,setBedroom]=useState("")
-    const [bathroom,setBathroom]=useState("")
-    const [price,setPrice]=useState("")
-    const [size,setSize]=useState("")
-    const [type,setType]=useState("")
-    const [location,setLocation]=useState("")
-    
-    const [file,setFile]=useState(null)
-    
-
-    const fetchCommunities = async()=>{
-      try{
-        const accessToken = localStorage.getItem("access_token");
-
-        if(!accessToken){
-              // Handle the case where the access token is not available
-          console.error('Access token not found')
-        }
-
-        const res=await axios.get(URL+"/api/communities/"+communityId,{
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
-        setRoomName(res.data.roomName)
-        setFloorsAvailable(res.data.floorsAvailable)
-        setDescription(res.data.description)
-        setFile(res.data.photo)
-       
-        setBedroom(res.data.bedroom)
-        setBathroom(res.data.bathroom)
-        setPrice(res.data.price)
-        setSize(res.data.size)
-        setType(res.data.type)
-        setLocation(res.data.location)
-
-      }
-      catch(err){
-        console.log(err)
-      }
+  const fetchCommunity = async () => {
+    try {
+      console.log("thisis it", CommunityId);
+      const res = await axios.get(URL + "/api/communities/" + CommunityId);
+      console.log(res);
+      setName(res.data.name);
+      setDescription(res.data.desc);
+      setFile(res.data.photo);
+      setStatus(res.data.status);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const handleUpdate=async (e)=>{
-      e.preventDefault()
-      const apartment = {
-        roomName,
-        description,
-        // username:user.username,
-        // userId:user._id,
-        // categories:cats
-        bedroom,
-        size,
-        type,
-        location,
-        price,
-        bathroom
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const Community = {
+      name,
+      description,
+      location,
+      status,
+      photo: imageUrl, // Append the image URL to the community payload
+    };
 
+    try {
+      const accessToken = localStorage.getItem("access_token");
+      if (!accessToken) {
+        console.error("Access token not found");
+        return; // Stop execution if access token is not found
       }
-
-      if(file){
-        const data=new FormData()
-        const filename=Date.now()+file.name
-        data.append("img",filename)
-        data.append("file",file)
-        apartment.photo=filename
-        // console.log(data)
-        //img upload
-        try{
-          const accessToken = localStorage.getItem("access_token");
-
-          if(!accessToken){
-                // Handle the case where the access token is not available
-            console.error('Access token not found')
-          }
-
-          const imgUpload = await axios.post(URL+"/api/upload",data,{
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          })
-          // console.log(imgUpload.data)
-        }
-        catch(err){
-          console.log(err)
-        }
-      }
-      //post upload
-     
-      try{
-        const accessToken = localStorage.getItem("access_token");
-
-        if(!accessToken){
-              // Handle the case where the access token is not available
-          console.error('Access token not found')
-        }
-
-        const res=await axios.put(URL+"/api/apartments/"+apartmentId,apartment,{
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
-        navigate("/apartmentscreated")
-        // console.log(res.data)
-
-      }
-      catch(err){
-        console.log(err)
-      }
+      await axios.put(URL + "/api/communities/" + CommunityId, Community, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      navigate(-1);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    
+  // Function to handle file upload
+  const handleFileUpload = async (e) => {
+    const file = e.target.files[0];
+    const data = new FormData();
+    const filename = Date.now() + file.name;
+    data.append("img", filename);
+    data.append("file", file);
 
-    useEffect(()=>{
-      fetchCommunities()
-    },[communityId])
+    try {
+      const url = await axios.post(URL + "/api/upload", data);
+      setImageUrl(url.data[0]); // Set the image URL in the state
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-
+  useEffect(() => {
+    fetchCommunity();
+  }, []); // Empty dependency array to execute only once when component mounts
 
   return (
     <div>
-        {/* <Navbar/> */}
-        <div className='px-6 md:px-[200px] mt-8'>
-            <div onClick={() => navigate(-1)} className="flex items-center space-x-3">
-        <SlArrowLeft />
-        <h1 className='font-bold md:text-2xl text-xl '>Back</h1>
+      <div className="px-6 md:px-[200px] mt-8">
+        <div
+          onClick={() => navigate(-1)}
+          className="flex items-center space-x-3"
+        >
+          <SlArrowLeft />
+          <h1 className="font-bold md:text-2xl text-xl ">Back</h1>
         </div>
-        <h1 className='font-bold md:text-2xl text-xl text-center '>Update an Apartment</h1>
-        <form className='w-full flex flex-col space-y-4 md:space-y-8 mt-4'>
-          <input onChange={(e)=>setRoomName(e.target.value)} value={roomName} type="text" placeholder='Enter Apartment Name' className='px-4 py-2 border outline-none text-gray-400'/>
-          <input onChange={(e)=>setFloorsAvailable(e.target.value)} value={floorsAvailable} type="text" placeholder='Enter post title' className='px-4 py-2 border outline-none text-gray-400'/>
-          <input onChange={(e)=>setBedroom(e.target.value)} value={bedroom} type="text" placeholder='Enter post title' className='px-4 py-2 border outline-none text-gray-400'/>
-          <input onChange={(e)=>setBathroom(e.target.value)} value={bathroom} type="text" placeholder='Enter post title' className='px-4 py-2 border outline-none text-gray-400'/>
-          <input onChange={(e)=>setSize(e.target.value)} value={size} type="text" placeholder='Enter post title' className='px-4 py-2 border outline-none text-gray-400'/>
-          <input onChange={(e)=>setType(e.target.value)} value={type} type="text" placeholder='Enter post title' className='px-4 py-2 border outline-none text-gray-400'/>
-          <input onChange={(e)=>setPrice(e.target.value)} value={price} type="text" placeholder='Enter post title' className='px-4 py-2 border outline-none text-gray-400'/>
-          <input onChange={(e)=>setLocation(e.target.value)} value={location} type="text" placeholder='Enter post title' className='px-4 py-2 border outline-none text-gray-400'/>
-          {/* <input onChange={(e)=>setSize(e.target.value)} value={size} type="text" placeholder='Enter post title' className='px-4 py-2 outline-none'/> */}
-          <input onChange={(e)=>setFile(e.target.files[0])} type="file"  className='px-4'/>
-          <div className='flex flex-col'>
-            <div className='flex items-center space-x-4 md:space-x-8'>
-              
-                
-            
-            
-            </div>
-          </div>
-          {/* <textarea onChange={(e)=>setDescription(e.target.value)} value={description} rows={15} cols={30} className='px-4 py-2 border outline-none' placeholder='Enter post description'/> */}
-          <button onClick={handleUpdate} className='bg-black w-full md:w-[20%] mx-auto text-white font-semibold px-4 py-2 md:text-xl text-lg'>Update Apartment</button>
+        <h1 className="font-bold md:text-2xl text-xl text-center ">
+          Update a Community
+        </h1>
+        <form className="w-full flex flex-col space-y-4 md:space-y-8 mt-4">
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            placeholder="Enter Community Name"
+            className="px-4 py-2 border outline-none text-gray-400"
+          />
+          <input
+            onChange={(e) => setStatus(e.target.value)}
+            value={status}
+            type="text"
+            placeholder="Enter status"
+            className="px-4 py-2 border outline-none text-gray-400"
+          />
+
+          <input
+            onChange={handleFileUpload} // Handle file upload when file input changes
+            type="file"
+            className="px-4"
+          />
+          <textarea
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            rows={15}
+            cols={30}
+            className="px-4 py-2 border outline-none"
+            placeholder="Enter post description"
+          />
+          <button
+            onClick={handleUpdate}
+            className="bg-black w-full md:w-[20%] mx-auto text-white font-semibold px-4 py-2 md:text-xl text-lg"
+          >
+            Update Community
+          </button>
         </form>
-
-        </div>
-        {/* <Footer/> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EditCommunity
+export default EditCommunity;
