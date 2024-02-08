ea9dde6de041ee1562b379d4b7b4fd8bc8185112
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-08 21:11:24 +0100

    Add active link highlighting to Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,31 +1,42 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Sidebar = () => {
-  return (
-    <div className="border h-screen w-[16%] flex flex-col justify-center items-center">
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState("");
 
+  useEffect(() => {
+    setActiveLink(location.pathname);
+  }, [location.pathname]);
+
+  const isActive = (link) => {
+    return link === activeLink ? "bg-green-400 text-white" : "";
+  };
+
+  return (
+    <div className="border h-screen w-[16%] flex flex-col justify-top items-center mt-8">
       <Link to="/admin/dashboard">
         <p className="text-green-800 text-2xl font-bold py-2">Admin</p>
       </Link>
       <div className="text-left mt-6 space-y-6">
-        <Link to={"/admin/dashboard"}>
-          <p>Dashboard</p>
-        </Link>
-        <Link to={"/admin/tenant"}>
-          <p className="mt-6">Tenant</p>
-        </Link>
-        <Link to={"/admin/unit"}>
-          <p className="mt-6">Units</p>
-        </Link>
-        <Link to={"/admin/community"}>
-          <p className="mt-6">Community</p>
-        </Link>
-        <Link to={"/admin/submarket"}>
-          <p className="mt-6">Submarket</p>
-        </Link>
-        <Link to={"/admin/reservation"}>
-          <p className="mt-6">Reservations</p>
-        </Link>
+        <p className={`px-4 py-2 rounded-lg ${isActive("/admin/dashboard")}`}>
+          <Link to={"/admin/dashboard"}>Dashboard</Link>
+        </p>
+        <p className={`px-4 py-2 rounded-lg ${isActive("/admin/tenant")}`}>
+          <Link to={"/admin/tenant"}>Tenant</Link>
+        </p>
+        <p className={`px-4 py-2 rounded-lg ${isActive("/admin/unit")}`}>
+          <Link to={"/admin/unit"}>Units</Link>
+        </p>
+        <p className={`px-4 py-2 rounded-lg ${isActive("/admin/community")}`}>
+          <Link to={"/admin/community"}>Community</Link>
+        </p>
+        <p className={`px-4 py-2 rounded-lg ${isActive("/admin/submarket")}`}>
+          <Link to={"/admin/submarket"}>Submarket</Link>
+        </p>
+        <p className={`px-4 py-2 rounded-lg ${isActive("/admin/reservation")}`}>
+          <Link to={"/admin/reservation"}>Reservations</Link>
+        </p>
       </div>
     </div>
   );
