c09240ad7aa24de9e48393725d802122866cfe29
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-13 00:46:22 +0100

    Remove commented out code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,52 +33,6 @@ import UnitTypesCreated from './pages/UnitTypesCreated'
 import InsideCommunity from './pages/InsideCommunity'
 import "react-toastify/dist/ReactToastify.css";
 
-
-
-// const App = () => {
-  
-//   return (
-//     <UserContextProvider>
-//       <ToastContainer position="top-center" limit={1} />
-
-//       <Routes>
-//         {/* CLIENT PAGES */}
-//         <Route exact path="/" element={<FrontPage />} />
-//         <Route exact path="/recoa/:id" element={<RecoaSquare />} />
-//         <Route exact path="/test" element={<Test />} />
-//         <Route exact path="/dashboard" element={<AdminDashboard />} />
-//         <Route exact path="/register" element={<Register />} />
-//         <Route exact path="/findcommunity/:id" element={<FindCommunity />} />
-//         <Route exact path="/clientlogin" element={<ClientLogin />} />
-//         <Route exact path="/about" element={<About />} />
-//         <Route exact path="/communities" element={<Communities />} />
-//         <Route exact path="/corperatepage" element={<CorperatePage />} />
-//         <Route exact path="/corperatelogin/:id" element={<CorperateLogin />} />
-//         <Route exact path="/insidecommunity/:id" element={<InsideCommunity />} />
-//         <Route exact path="/reserve" element={<Reserve />} />
-
-//         {/* ADMIN PAGES */}
-//         <Route exact path="/admin/dashboard" element={<AdminPage />} />
-//         <Route exact path="/admin/login" element={<Login />} />
-//         <Route exact path="/admin/tenant" element={<CreateTenant />} />
-//         <Route exact path="/admin/submarket" element={<CreateSubmarket />} />
-//         <Route exact path="/admin/community" element={<CreateCommunity />} />
-//         <Route exact path="/admin/reservation" element={<CreateReservation />} />
-//         <Route exact path="/admin/unit" element={<CreateUnitType />} />
-//         <Route exact path="/admin/tenant/view" element={<TenantsCreated />} />
-//         <Route exact path="/admin/community/view" element={<CommunitiesCreated />} />
-//         <Route exact path="/admin/reservation/view" element={<ReservationsCreated />} />
-//         <Route exact path="/admin/submarket/view" element={<SubmarketsCreated />} />
-//         <Route exact path="/admin/unit/view" element={<UnitTypesCreated />} />
-//         <Route exact path="/admin/submarket/edit/:id" element={<EditSubmarket />} />
-//         <Route exact path="/admin/unittype/edit/:id" element={<EditUnitType />} />
-//         <Route exact path="/admin/reservation/edit/:id" element={<EditReservation />} />
-//         <Route exact path="/admin/community/edit/:id" element={<EditCommunity />} />
-//       </Routes>
-//     </UserContextProvider>
-//   );
-// }
-
 const App = () => {
   return (
     <UserContextProvider>
