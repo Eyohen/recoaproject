e73ad098002674bc3a12d8e8b74bea1a03353a40
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-17 05:51:02 +0100

    Refactor FloorPlans component to improve code readability and performance

--- src/components/FloorPlans.jsx
+++ src/components/FloorPlans.jsx
@@ -1,41 +1,33 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Floorcard from "./Floorcard";
-import { Link, useLocation, useParams, useNavigate } from "react-router-dom";
-import { UserContext } from "../context/UserContext";
-// import Loader from '../components/Loader'
-import { IF, URL } from "../url";
+import { useLocation, useParams } from "react-router-dom";
+import Loader from "../components/Loader"; // Make sure to uncomment this
+import { URL } from "../url";
 
 const FloorPlans = () => {
   const { search } = useLocation();
-  // console.log(search)
   const submarketId = useParams().id;
   const [communities, setCommunities] = useState([]);
   const [noResults, setNoResults] = useState(false);
   const [loader, setLoader] = useState(false);
-  const { user } = useContext(UserContext);
 
   const fetchPosts = async () => {
     setLoader(true);
     try {
-      const res = await axios.get(URL + "/api/communities/" + search);
+      const res = await axios.get(`${URL}/api/communities${search}`);
       console.log("floorplandata", res.data);
-      // setCommunities(res.data)
-      setCommunities(() => {
-        const filteredCommunities = res.data.filter(
-          (community) => community.submarket._id === submarketId
-        );
-        return filteredCommunities;
-      });
-      if (res.data.length === 0) {
-        setNoResults(true);
-      } else {
-        setNoResults(false);
-      }
-      setLoader(false);
+      const filteredCommunities = res.data.filter(
+        (community) => community.submarket._id === submarketId
+      );
+      setCommunities(filteredCommunities);
+
+      setNoResults(filteredCommunities.length === 0);
     } catch (err) {
       console.log(err);
-      setLoader(true);
+      setNoResults(true); // Consider setting noResults to true in case of error
+    } finally {
+      setLoader(false); // Ensure loader is turned off both on success and failure
     }
   };
 
@@ -45,17 +37,21 @@ const FloorPlans = () => {
 
   return (
     <div className="bg-green-800 p-6 rounded-3xl max-w-6xl mx-auto mt-10">
-      <p className="text-white text-3xl text-center mt-3 font-medium ">
+      <p className="text-white text-3xl text-center mt-3 font-medium">
         Floor Plans
       </p>
 
-      <div className="flex space-x-3 md:flex-row flex-col justify-center items-center overflow-x-scroll">
-        {communities.map((community) => (
-          <>
+      {loader ? (
+        <Loader /> // Display Loader component when data is being fetched
+      ) : noResults ? (
+        <p className="text-white text-center">No floor plans found.</p> // Display message when no results are found
+      ) : (
+        <div className="flex space-x-3 md:flex-row flex-col justify-center items-center overflow-x-scroll">
+          {communities.map((community) => (
             <Floorcard key={community._id} community={community} />
-          </>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
