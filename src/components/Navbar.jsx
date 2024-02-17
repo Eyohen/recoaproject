86adba651475df74ecec0c8f133e4d245d0615ef
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-17 05:51:52 +0100

    Refactor Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,40 @@
-import React,{ useContext, useEffect, useState } from "react"
-import { Link } from 'react-router-dom'
-import { UserContext } from "../context/UserContext"
-import axios from "axios"
-import { IF, URL } from "../url"
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
- 
-  const {user}=useContext(UserContext)
- 
-
-
-  
   return (
-    <div className='h-12 items-center'>
-    <div className='flex justify-between p-2 px-6'>
-   <Link to={'/'}> <p className='text-green-700 font-bold text-3xl'>Recoa</p></Link>
-    <div className='flex space-x-6'>
-    <Link to="/about"><p className='hidden md:block text-green-700 text-lg font-medium hover:bg-green-800 hover:text-white hover:rounded-full hover:px-1.5 ' >About us</p></Link>
-    {/* <Link to={user?'/findapartment':"/clientlogin" }><p className='text-green-700 text-lg font-medium hover:bg-green-800 hover:text-white hover:rounded-full hover:px-1.5'>Find your Apartment</p></Link> */}
-    <Link to={'/findcommunity'}><p className='hidden md:block text-green-700 text-lg font-medium hover:bg-green-800 hover:text-white hover:rounded-full hover:px-1.5 '>Find your Apartment</p></Link>
-    <Link to="/communities"><p className='hidden md:block text-green-700 text-lg font-medium hover:bg-green-800 hover:text-white hover:rounded-full hover:px-1.5'>Communities</p></Link>
-    <Link to="/corperatepage"><p className='hidden md:block text-green-700 text-lg font-medium hover:bg-green-800 hover:text-white hover:rounded-full hover:px-1.5'>Corperate tenants</p></Link>
+    <div className="h-12 items-center">
+      <div className="flex justify-between p-2 px-6">
+        <Link to={"/"}>
+          {" "}
+          <p className="text-green-700 font-bold text-3xl">Recoa</p>
+        </Link>
+        <div className="flex space-x-6">
+          <Link to="/about">
+            <p className="hidden md:block text-green-700 text-lg font-medium hover:bg-green-800 hover:text-white hover:rounded-full hover:px-1.5 ">
+              About us
+            </p>
+          </Link>
+          {/* <Link to={user?'/findapartment':"/clientlogin" }><p className='text-green-700 text-lg font-medium hover:bg-green-800 hover:text-white hover:rounded-full hover:px-1.5'>Find your Apartment</p></Link> */}
+          <Link to={"/findcommunity"}>
+            <p className="hidden md:block text-green-700 text-lg font-medium hover:bg-green-800 hover:text-white hover:rounded-full hover:px-1.5 ">
+              Find your Apartment
+            </p>
+          </Link>
+          <Link to="/communities">
+            <p className="hidden md:block text-green-700 text-lg font-medium hover:bg-green-800 hover:text-white hover:rounded-full hover:px-1.5">
+              Communities
+            </p>
+          </Link>
+          <Link to="/corperatepage">
+            <p className="hidden md:block text-green-700 text-lg font-medium hover:bg-green-800 hover:text-white hover:rounded-full hover:px-1.5">
+              Corperate tenants
+            </p>
+          </Link>
+        </div>
+      </div>
     </div>
+  );
+};
 
-
-
-    
-    </div>
-</div>
-  )
-}
-
-export default Navbar
+export default Navbar;
