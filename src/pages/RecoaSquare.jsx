9a02cd1896ffc04d610dcd7153831d11c7833618
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-17 05:53:18 +0100

    Refactor RecoaSquare component

--- src/pages/RecoaSquare.jsx
+++ src/pages/RecoaSquare.jsx
@@ -1,10 +1,8 @@
-import React, { useContext, useEffect, useState } from "react";
-import VI from "../assets/VictoriaIsland.png";
+import { useEffect, useState } from "react";
 import FloorPlans from "../components/FloorPlans";
 import axios from "axios";
-import { URL, IF } from "../url";
-import Loader from "../components/Loader";
-import { useNavigate, useParams } from "react-router-dom";
+import { URL } from "../url";
+import { useParams } from "react-router-dom";
 
 const RecoaSquare = () => {
   const submarketId = useParams().id;
@@ -37,10 +35,6 @@ const RecoaSquare = () => {
       <p className="text-3xl text-green-800 mt-8 text-center">
         Select Your FLoor Plans Below{" "}
       </p>
-      {/* <div className='flex justify-evenly max-w-6xl mx-auto mt-9 items-center'>
-            <p>Loreem Ipsum has been the industry's standard <br/> dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen <br /> book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            <img src={VI} alt='' className='rounded-xl w-[600px] h-[300px] '/>
-        </div> */}
 
       <div className=" justify-evenly max-w-6xl mx-auto mt-9 items-center mb-12">
         <p className="text-center">{subMarket.description}</p>
