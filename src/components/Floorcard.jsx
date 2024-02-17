e5fda3fdf5d8935aa611d5aae22550b921871636
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-17 05:50:46 +0100

    Refactor Floorcard component

--- src/components/Floorcard.jsx
+++ src/components/Floorcard.jsx
@@ -1,12 +1,9 @@
-import React, { useState, useEffect } from "react";
-import VI from "../assets/VictoriaIsland.png";
-import { Link } from "react-router-dom";
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 import "./Modal.css";
 import { HiPlusSmall } from "react-icons/hi2";
 import { HiMinusSmall } from "react-icons/hi2";
-import { IF, URL } from "../url";
-import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+
 
 const Floorcard = ({ community }) => {
   const [modal, setModal] = useState(false);
@@ -24,67 +21,55 @@ const Floorcard = ({ community }) => {
     setCount(count - 1);
   };
 
-  // useEffect(()=>{
-  //   fetchProfile()
-  // },[param])
 
   return (
-    <div>
-      <p className="text-center text-lg">{community?.name}</p>
+
+      <div className='relative mt-6'>
+
+     <img src={community.photo || "https://www.svgrepo.com/show/340721/no-image.svg"} alt='' className='w-[450px] h-[250px] object-cover rounded-3xl brightness-75'/>
+
+        <div className="absolute top-6 flex w-full space-x-6 justify-center">
+      <p className='text-white font-medium text-md '>{community?.type}</p>
+      <p className='text-white font-medium text-md'>{community?.bathroom} Bath</p>
+      <p className='text-white font-medium text-md'>{community?.size} sq.m</p>
+    </div>
+      <div className="absolute top-16 flex w-full justify-center">
+        <div>
+      <p className='text-white font-medium text-md '>{community?.name}</p>
+      <p className='text-white font-medium text-2xl '><span className='text-3xl'>{community?.floorsAvailable}</span> available</p>
+      </div>
+    </div>
+    <div className="absolute top-32 flex w-full justify-center">
+      <button className='bg-white text-green-700 font-medium text-2xl  rounded-full px-2 items-center'>starting at ${community.price}/annum</button>
     </div>
 
-    //   <div className='relative mt-6'>
+    {modal && (
+          <div className="modal">
+            <div onClick={toggleModal} className="overlay"></div>
+            <div className="modal-content rounded-xl">
+              <p className='text-center text-2xl text-green-700'>community A</p>
+              <p className='text-center mt-3'>{community.floorsAvailable - count} floors Remaining</p>
+              <p  className='text-center mt-3'>Amount: {community.price * count}</p>
+              <div className='flex space-x-4 items-center justify-center mt-3'>
+              <button onClick={decrease} className='bg-green-700 rounded-full px-2'><HiMinusSmall /></button>
+              <p>
+               {count}
+              </p>
+              <button onClick={increase} className='bg-green-700 rounded-full px-2'><HiPlusSmall /></button>
+              </div>
+              {/* <div className='items-center justify-center '> */}
+              <button className='bg-green-700 px-2 rounded-full mx-32 mt-6' onClick={toggleModal}>Submit</button>
+              {/* </div> */}
+              <button className="close-modal" onClick={toggleModal}>
+                CLOSE
+              </button>
 
-    //  {/* <div class="absolute top-6 flex w-full">
-    //   <p className='text-white absolute font-medium text-md ml-20'>Now Pre-leasing</p>
-    // </div> */}
-    //  <img src={IF+community.photo ? IF+community.photo : "https://www.svgrepo.com/show/340721/no-image.svg"} alt='' className='w-[450px] h-[250px] object-cover rounded-3xl brightness-75'/>
+            </div>
 
-    //     <div class="absolute top-6 flex w-full space-x-6 justify-center">
-    //   <p className='text-white font-medium text-md '>{community?.type}</p>
-    //   <p className='text-white font-medium text-md'>{community?.bathroom} Bath</p>
-    //   <p className='text-white font-medium text-md'>{community?.size} sq.m</p>
-    // </div>
-    //   <div class="absolute top-16 flex w-full justify-center">
-    //     <div>
-    //   <p className='text-white font-medium text-md '>{community?.name}</p>
-    //   <p className='text-white font-medium text-2xl '><span className='text-3xl'>{community?.floorsAvailable}</span> available</p>
-    //   </div>
-    // </div>
-    // <div class="absolute top-32 flex w-full justify-center">
-    //   <button className='bg-white text-green-700 font-medium text-2xl  rounded-full px-2 items-center'>starting at ${community?.price}/annum</button>
-    // </div>
-    // {/* <div class="absolute top-[190px] flex w-full justify-center">
-    //   <button onClick={toggleModal} className='text-white font-medium text-2xl border-2 border-white rounded-full px-2'>Reserve</button>
-    // </div> */}
+          </div>
+        )}
 
-    // {modal && (
-    //       <div className="modal">
-    //         <div onClick={toggleModal} className="overlay"></div>
-    //         <div className="modal-content rounded-xl">
-    //           <p className='text-center text-2xl text-green-700'>community A</p>
-    //           <p className='text-center mt-3'>{community.floorsAvailable - count} floors Remaining</p>
-    //           <p  className='text-center mt-3'>Amount: {community.price * count}</p>
-    //           <div className='flex space-x-4 items-center justify-center mt-3'>
-    //           <button onClick={decrease} className='bg-green-700 rounded-full px-2'><HiMinusSmall /></button>
-    //           <p>
-    //            {count}
-    //           </p>
-    //           <button onClick={increase} className='bg-green-700 rounded-full px-2'><HiPlusSmall /></button>
-    //           </div>
-    //           {/* <div className='items-center justify-center '> */}
-    //           <button className='bg-green-700 px-2 rounded-full mx-32 mt-6' onClick={toggleModal}>Submit</button>
-    //           {/* </div> */}
-    //           <button className="close-modal" onClick={toggleModal}>
-    //             CLOSE
-    //           </button>
-
-    //         </div>
-
-    //       </div>
-    //     )}
-
-    //   </div>
+      </div>
   );
 };
 
