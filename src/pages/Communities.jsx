4a4aafc7d855437326075125d59b32c35f7d07c9
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-19 03:54:10 +0100

    Refactor Communities.jsx component and add conditional rendering for waitlist and reservation buttons

--- src/pages/Communities.jsx
+++ src/pages/Communities.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import Navbar from "../components/Navbar";
 import { URL } from "../url";
-import CompanyCard from "../components/CompanyCard";
 
 const fetchAPI = async (endpoint) => {
   try {
     const response = await axios.get(`${URL}${endpoint}`);
+    console.log("response", response.data);
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -16,41 +16,63 @@ const fetchAPI = async (endpoint) => {
   }
 };
 
-
 const CommunityCard = ({ community }) => {
-  const [companies, setCompanies] = useState([]);
-
-  useEffect(() => {
-    (async () => setCompanies(await fetchAPI("/api/tenants/")))();
-  }, []);
+  const isNowPreleasing = community.status === "Now pre-leasing";
 
   return (
     <div className="border-2 border-emerald-400 p-6 rounded-lg shadow mx-auto max-w-4xl">
+      <img
+        src={community.photo}
+        alt={community.name}
+        className="w-full h-auto mt-4 rounded-md"
+      />
       <p className="text-emerald-800 text-3xl text-center mt-8">
         {community.name}
       </p>
       <p className="text-center mt-4 px-4 md:px-16">{community.desc}</p>
-      <div className="mt-9 text-center">
-        <p className="text-emerald-400 border-2 border-emerald-400 px-2 py-2 rounded-md text-lg">
-          Preleasing Open to Corporate Tenants
-        </p>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-center items-center mt-4 p-4">
-          {companies.map((company) => (
-            <Link
-              to={`/corporatelogin/${company._id}`}
-              key={company._id}
-              className="hover:underline flex justify-center w-full" // Ensure the link takes full width to center content
-            >
-              <CompanyCard company={company} />
-            </Link>
-          ))}
+      <div className="mt-8 px-4 py-6 max-w-4xl mx-auto bg-green-50 rounded-lg shadow">
+        <div className="flex justify-between items-center">
+          <div>
+            <h2 className="text-2xl font-semibold text-green-800">
+              Community Details
+            </h2>
+            <p className="mt-2 text-lg text-green-700">
+              Status: {community.status} {community.openingDate}
+            </p>
+          </div>
+          <div className="text-right">
+            <p className="text-md text-gray-600">
+              Location: {community.submarket.location}
+            </p>
+          </div>
         </div>
+        <p className="mt-4 text-gray-600">{community.desc}</p>
+        <div className="mt-4">
+          <h3 className="text-xl font-semibold text-green-700">
+            Submarket Information
+          </h3>
+          <p className="text-md text-gray-600">
+            Name: {community.submarket.name}
+          </p>
+          <p className="mt-2 text-gray-600">
+            {community.submarket.description}
+          </p>
+        </div>
+        {!isNowPreleasing && (
+          <Link to={`/waitlist/${community._id}`}>
+            <button className="mt-6 bg-green-800 text-white px-4 py-2 rounded-md">
+              Join Waitlist
+            </button>
+          </Link>
+        )}
+        {isNowPreleasing && (
+          <Link to={`/community/${community._id}`}>
+            <button className="mt-6 bg-blue-800 text-white px-4 py-2 rounded-md">
+              Reserve Now
+            </button>
+          </Link>
+        )}
       </div>
-
-      <Link to="/reserve" className="block mt-16">
-        <p className="text-emerald-400 text-lg underline">Join WaitList</p>
-      </Link>
     </div>
   );
 };
@@ -59,7 +81,16 @@ const Communities = () => {
   const [communities, setCommunities] = useState([]);
 
   useEffect(() => {
-    (async () => setCommunities(await fetchAPI("/api/communities/")))();
+    const currentUserStr = localStorage.getItem("currentUser");
+    const currentUser = currentUserStr ? JSON.parse(currentUserStr) : null;
+    if (currentUser && currentUser.role === "tenant" && currentUser.tenant) {
+      (async () =>
+        setCommunities(
+          await fetchAPI(`/api/communities/tenant/${currentUser.tenant}`)
+        ))();
+    } else {
+      (async () => setCommunities(await fetchAPI("/api/communities/")))();
+    }
   }, []);
 
   return (
@@ -67,9 +98,9 @@ const Communities = () => {
       <Navbar />
       <div className="flex flex-col gap-y-6 mt-6 mx-auto max-w-4xlw">
         {communities.map((community) => (
-          <Link to={`/insidecommunity/${community._id}`} key={community._id}>
+          <>
             <CommunityCard community={community} />
-          </Link>
+          </>
         ))}
       </div>
     </div>
